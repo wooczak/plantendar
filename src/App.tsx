79221b6d9b0b40248adb5e90d9acf59621014c10
wooczak <kacper.luczak99@gmail.com>
2022-06-19 18:39:46 +0200

    Initializing the project with Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>Welcome to Plantendar</h2>
+      <p>The best app for you!</p>
+    </div>
+  )
+}
+
+export default App
